7d54d0a3b266b1c789109ce51de2e4c340529a3a
chuck <efbmachine06@gmail.com>
2020-04-15 09:34:00 +0200

    changed mongo from local to mongo

--- config/mongoose.js
+++ config/mongoose.js
@@ -9,9 +9,9 @@ require('../models/product');
 
 //set up default mongoose connection
 
-var mongoDB = 'mongodb+srv://<username>:<password>@cluster0-y2yv1.mongodb.net/glovo241?retryWrites=true&w=majority';
+var mongoDB = 'mongodb+srv://glovo241:NJtXnDLb46XKeak@cluster0-y2yv1.mongodb.net/glovo241?retryWrites=true&w=majority';
 var local = 'mongodb://localhost:27017/test'
-mongoose.connect(local);
+mongoose.connect(mongoDB);
 console.log('gucci gang');
 //get the default connection
 var db = mongoose.connection;
