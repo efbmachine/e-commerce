88a85ae03febfcebe262770f5ca4c33caca3b923
chuck <efbmachine06@gmail.com>
2020-05-10 22:20:05 +0200

    fixed db issue

--- config/mongoose.js
+++ config/mongoose.js
@@ -10,7 +10,7 @@ require('../models/product');
 //set up default mongoose connection
 var realMDB = 'mongodb+srv://webApp:NJtXnDLb46XKeak@cluster0-fizxw.mongodb.net/glovo241?retryWrites=true&w=majority'
 var local = 'mongodb://localhost:27017/test'
-mongoose.connect(local, {useUnifiedTopology:true, useNewUrlParser:true});
+mongoose.connect(realMDB, {useUnifiedTopology:true, useNewUrlParser:true});
 console.log('gucci gang');
 //get the default connection
 var db = mongoose.connection;
